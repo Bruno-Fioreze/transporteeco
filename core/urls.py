14dfbbe683eec9daa68ee8d301e7c0f9ca75b5a3
Bruno Fioreze <bruno-fioreze-@hotmail.com>
2025-02-02 03:12:36 -0300

    feat: added dependences

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,12 @@ from core.views import view_home
 
 urlpatterns = [
     path('', view_home),
+    path('retirada-entulho-sao-paulo', view_home),
+    path('transporte-entulho-sao-paulo', view_home),
+    path('coleta-entulho-sao-paulo', view_home),
+    path('coleta-entulho-sp', view_home),
+    path('transporte-entulho-sp', view_home),
+    path('retirada-entulho-sp', view_home),
+    path('retira-entulho-sp', view_home),
+    path('retira-entulho-sao-paulo', view_home),
 ]
