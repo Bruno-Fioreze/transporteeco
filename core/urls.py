b78f334193f819a41ceaeb04bf7f8ddf6c777a0e
Bruno Fioreze <bruno-fioreze-@hotmail.com>
2025-02-02 22:33:20 -0300

    feat: added sitemap urls

--- core/urls.py
+++ core/urls.py
@@ -14,4 +14,12 @@ urlpatterns = [
     path('retira-entulho-sp', view_home),
     path('retira-entulho-sao-paulo', view_home),
     path('sitemap.xml', sitemap, name='sitemap'),
+    path('retira-entulho-sao-paulo/sitemap.xml', sitemap),
+    path('retira-entulho-sp/sitemap.xml', sitemap),
+    path('retirada-entulho-sp/sitemap.xml', sitemap),
+    path('transporte-entulho-sp/sitemap.xml', sitemap),
+    path('coleta-entulho-sp/sitemap.xml', sitemap),
+    path('coleta-entulho-sao-paulo/sitemap.xml', sitemap),
+    path('transporte-entulho-sao-paulo/sitemap.xml', sitemap),
+    path('retirada-entulho-sao-paulo/sitemap.xml', sitemap),
 ]
