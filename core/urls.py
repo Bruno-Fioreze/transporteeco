72b15119f6a1828cd2a35a869e7fabe2ce555f37
Bruno Fioreze <bruno-fioreze-@hotmail.com>
2025-02-09 07:52:48 -0300

    fix: adjust text page

--- core/urls.py
+++ core/urls.py
@@ -1,20 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
-from core.views import view_home
+from core.views import view_home, view_home_teste
 from core.sitemaps import sitemap
 from core.robots import robots 
 
 urlpatterns = [
     path('', view_home),
     path('retirada-entulho-sao-paulo', view_home),
-    path('retirada-entulho-sao-paulo/', view_home),
-    path('transporte-entulho-sao-paulo', view_home),
-    path('coleta-entulho-sao-paulo', view_home),
-    path('coleta-entulho-sp', view_home),
-    path('transporte-entulho-sp', view_home),
-    path('retirada-entulho-sp', view_home),
-    path('retira-entulho-sp', view_home),
-    path('retira-entulho-sao-paulo', view_home),
+    path('retirada-entulho-sao-paulo/', view_home_teste),
+    path('transporte-entulho-sao-paulo', view_home_teste),
+    path('coleta-entulho-sao-paulo', view_home_teste),
+    path('coleta-entulho-sp', view_home_teste),
+    path('transporte-entulho-sp', view_home_teste),
+    path('retirada-entulho-sp', view_home_teste),
+    path('retira-entulho-sp', view_home_teste),
+    path('retira-entulho-sao-paulo', view_home_teste),
     path('sitemap.xml', sitemap, name='sitemap'),
     re_path(r'^robots\.txt', robots),
     path('retira-entulho-sao-paulo/sitemap.xml', sitemap),
--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def view_home(request):
     return render(request, 'home.html')
+
+def view_home_teste(request):
+    return render(request, 'home.html')
